9b03d8b28c54e94d033787a34927b4fdbb8b95c5
Jonathan Guthrie <jono@perfect.org>
2016-09-25 00:18:06 -0400

    adding joins struct

---
+++ Sources/PerfectCRUD/Joins.swift
@@ -0,0 +1,33 @@
+//
+//  Joins.swift
+//  PerfectCRUD
+//
+//  Created by Jonathan Guthrie on 2016-09-24.
+//
+//
+
+import Foundation
+
+public struct DataSourceJoin {
+	public var table:			String		= ""
+	public var direction:		JoinType
+	public var onCondition:		String		= ""
+
+	public init() {
+		direction = .normal
+	}
+	public init(table: String,  onCondition: String = "", direction: JoinType = .normal) {
+		self.table = table
+		self.direction = direction
+		self.onCondition = onCondition
+	}
+}
+
+public enum JoinType {
+	case INNER
+	case OUTER
+	case LEFT
+	case RIGHT
+	case STRAIGHT
+	case normal
+}
