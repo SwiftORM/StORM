7bdc9635b2cbdc662364fda195e8c44839be70ca
Jonathan Guthrie <jono@perfect.org>
2016-09-30 15:45:13 -0400

    add error stub

---
+++ Sources/PerfectCRUD/PerfectCRUDError.swift
@@ -0,0 +1,13 @@
+//
+//  PerfectCRUDErrors.swift
+//  PerfectPostgresCRUD
+//
+//  Created by Jonathan Guthrie on 2016-09-26.
+//
+//
+
+public enum PerfectCRUDError: String {
+	case database = "No Database Specified"
+	case error = "Error"
+	case noError = "No Error"
+}
