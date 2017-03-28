10e1a8a56109897372ed91a11c2066d025a75954
Jonathan Guthrie <jono@perfect.org>
2017-03-28 11:05:59 -0400

    update to use major version only

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 	name: "StORM",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0)
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2)
 	],
 	exclude: []
 )
