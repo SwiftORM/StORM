aa28a85065ccdc38fa43c81db1a6273f7e711859
Jonathan Guthrie <jono@guthrie.net.nz>
2018-03-02 11:16:03 -0500

    Fix missing string

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-08-13 18:42:55 +0000
+// Date: 2018-03-02 16:12:45 +0000
 import PackageDescription
 let package = Package(
 	name: "StORM",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+		.Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 1),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
 	]
 )
--- Sources/StORM/Extract.swift
+++ Sources/StORM/Extract.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftMoment
+import SwiftString
 
 extension StORM {
 
