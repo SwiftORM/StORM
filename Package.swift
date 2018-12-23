ab848b0e46e57a6c352fc2d44186ee5faf8f5142
Ryan <ryan@buckettechnologies.com>
2018-12-23 14:29:07 -0500

    Updating the package file to make it line up with everything

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
 // swift-tools-version:4.0
 import PackageDescription
 let package = Package(
-	name: "StORM",
+    name: "StORM",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "StORM",
             targets: ["StORM"]),
     ],
-	dependencies: [
+    dependencies: [
         .package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
         .package(url: "https://github.com/ryancoyne/SwiftMoment.git", from: "1.0.0"),
-		.package(url: "https://github.com/ryancoyne/SwiftString.git", from: "2.0.0"),
-	],
+	.package(url: "https://github.com/ryancoyne/SwiftString.git", from: "2.0.0"),
+    ],
     targets: [
         .target(
             name: "StORM",
