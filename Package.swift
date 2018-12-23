42d6efe857bc9b42edd6f5b53f3ee02b2c90009a
Ryan <ryan@buckettechnologies.com>
2018-12-23 14:27:06 -0500

    Updating the StORM package to use my packages that use the updated package.swift file

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     ],
 	dependencies: [
         .package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
-        .package(url: "https://github.com/iamjono/SwiftMoment.git", from: "1.0.0"),
-		.package(url: "https://github.com/iamjono/SwiftString.git", from: "2.0.0"),
+        .package(url: "https://github.com/ryancoyne/SwiftMoment.git", from: "1.0.0"),
+		.package(url: "https://github.com/ryancoyne/SwiftString.git", from: "2.0.0"),
 	],
     targets: [
         .target(
