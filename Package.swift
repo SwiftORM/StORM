3b67e22f6ceae0c23047d6b1e21f9f88d089c6af
Ryan <ryan@buckettechnologies.com>
2018-12-23 13:42:53 -0500

    Updatingg the package file

--- Package.swift
+++ Package.swift
@@ -2,6 +2,12 @@
 import PackageDescription
 let package = Package(
 	name: "StORM",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "StORM",
+            targets: ["StORM"]),
+    ],
 	dependencies: [
         .package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
         .package(url: "https://github.com/iamjono/SwiftMoment.git", from: "1.0.0"),
