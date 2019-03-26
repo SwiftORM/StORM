fe48e5f42162c3785f4663e0023fb5558d6bbf1a
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 12:20:00 -0400

    Updated package file and removed dep

--- Package.swift
+++ Package.swift
@@ -1,12 +1,20 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2018-03-02 16:12:45 +0000
 import PackageDescription
 let package = Package(
 	name: "StORM",
-	targets: [],
+	products: [
+		.library(name: "StORM", targets: ["StORM"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
-		.Package(url: "https://github.com/iamjono/SwiftMoment.git", majorVersion: 1),
-		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
+		.package(url: "https://github.com/iamjono/SwiftMoment.git", from: "1.0.0")
+	],
+	targets: [
+		.target(name: "StORM", dependencies: [
+			"SwiftMoment",
+			"PerfectLib"
+		])
 	]
 )
